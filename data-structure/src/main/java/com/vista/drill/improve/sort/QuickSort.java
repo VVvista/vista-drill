3465002b76f3cfe9a80e8eb083e2da1521a190bb
VVvista <wentingting_vista@163.com>
2020-04-30 15:58:58 +0800

    希尔排序

--- data-structure/src/main/java/com/vista/drill/improve/sort/QuickSort.java
+++ data-structure/src/main/java/com/vista/drill/improve/sort/QuickSort.java
@@ -6,6 +6,12 @@ package com.vista.drill.improve.sort;
  * 2.利用pivot将序列分割成2个子序列： 小于pivot pivot 大于pivot
  * 3.对子序列重复以上操作，知道不能再分割(子序列中只含有一个元素)
  * 本质：逐步将每个元素当作轴点元素,即确定每个轴点元素的位置
+ * <p>
+ * 时间复杂度：
+ * 在轴点左右元素比较均匀的情况下，此时也是最好的情况：O(nlogn)
+ * 若轴点左右元素极度不均匀的情况，最坏情况：O(n^2)
+ * 空间复杂度：O(logn)
+ * 不稳定排序
  *
  * @author Wen TingTing by 2020/4/29
  */
@@ -21,12 +27,13 @@ public class QuickSort extends Sort {
 
     /**
      * 对序列进行分割，并确定轴点元素位置
+     * 递归实现
      *
      * @param begin
      * @param end
      */
     private void sort(int begin, int end) {
-        if(end-begin<2) return;// 当数组至少有2个元素时才进行快速排序
+        if (end - begin < 2) return;// 当数组至少有2个元素时才进行快速排序
         // 获取轴点元素位置
         int mid = pivotIndex(begin, end);
         // 对[begin,index) [index,end) 再进行快速排序
@@ -35,35 +42,54 @@ public class QuickSort extends Sort {
     }
 
     /**
-     * 对子序列进行分割，并将轴点元素放置在合适位置
-     * 假设：轴点元素为0元素位置
+     * 依次循环比较每个元素，将元素分成2个部分，确定轴点元素放置在合适位置
+     * 1.选取数组范围内的首元素为轴点元素，备份轴点元素element
+     * 2.分别循环移动 begin 和 end指针
+     * 先从end开始：
+     * 1.若：array[end] > element,end--;尾指针前移一位
+     * 2.若：array[end] < element,array[begin]=array[end],begin++;将end位置元素移动begin位置，并begin后移一位
+     * 3.若：array[begin] < element,begin++;头指针前移一位
+     * 4.若：array[begin] > element,array[end]=array[begin],end--;将begin位置元素移动到end位置，并end前移一位
+     * 重复以上步骤，直至begin=end结束，此时begin位置即为轴点元素待插入位置，最后将轴点插入该位置
+     * 注：若元素与轴点元素相同，仍然是将元素移动另一方，不管是begin方还是end方；此处是为了避免快速排序出现最坏情况，因此快速排序为不稳定排序
+     * （若序列中所有元素都与轴点元素相等时，也能将序列分割成2个均匀的子序列）
+     * <p>
+     * 最坏的情况：每次选取的轴点元素都为最大最小值，使得数组范围内的元素都统一左移或右移一位；
+     * 优化：随机选取数组中的元素作为轴点元素
+     * 当随机选取一个轴点元素时，仍将轴点元素与首元素交换，此时轴点元素就在首位置，之后的操作跟常规操作一样
      *
      * @param begin
      * @param end
      * @return
      */
     private int pivotIndex(int begin, int end) {
+/*   随机选取一个轴点元素
+        // 为了降低最坏情况(O(n^2)的时间复杂度)的出现概率，随机选择一个元素跟begin位置进行交换
+        swap(begin, begin + (int) (Math.random() * (end - begin)));
+        */
+
         // 备份轴点元素
         Integer pivot = array[begin];
+        end--;// end指向最后一个元素
         while (begin < end) {
-            while (begin<end) {
-                if (array[end] > pivot) {
-                    end--;
-                } else {
+            while (begin < end) {
+                if (array[end] > pivot) {// 右边元素 > 轴点元素
+                    end--;// 只位移，不进行元素交换
+                } else {// 右边元素 <= 轴点元素
                     array[begin++] = array[end];
-                    break;
+                    break; //执行完一次操作后，需要掉头，所以执行一个break
                 }
             }
-            while (begin<end) {
-                if (array[begin] < pivot) {
+            while (begin < end) {
+                if (array[begin] < pivot) {// 左边元素 < 轴点元素
                     begin++;
-                } else {
+                } else {// 左边元素 >= 轴点元素
                     array[end--] = array[begin];
-                    break;
+                    break;// 通过两个break，能实现两个while交替执行。
                 }
             }
         }
-        array[begin] = pivot;
-        return begin;
+        array[begin] = pivot;// 将轴点元素放入最终的位置
+        return begin;// 返回轴点元素的位置
     }
 }
---
+++ data-structure/src/main/java/com/vista/drill/improve/sort/ShellSort.java
@@ -0,0 +1,89 @@
+package com.vista.drill.improve.sort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 希尔排序
+ * 将序列看作一个矩阵，分成m列，逐列进行排序
+ * m从某个正数逐渐减为1，当m=1时，整个序列将完全有序。因此也被称为递减增量排序
+ * 矩阵的列数取决于步长序列：不同的步长序列，执行效率也不同
+ * 底层使用插入排序对每列进行排序，因此希尔排序也可看作插入排序的改进版
+ * <p>
+ * 注：希尔排序每次选取一定的步长进行插入排序，从而减少逆序对的数量，
+ * 所以当步长为1时，序列中的逆序对数量已非常少，再进行全量插入排序，交换的次数已非常少
+ * <p>
+ * 时间复杂度：
+ * 平均复杂度：取决于步长序列
+ * 最坏复杂度：O(n^2)
+ * 最好复杂度：O(n)(步长序列只有1，且序列完全有序)
+ * 空间复杂度：O(1)
+ * 不稳定排序
+ *
+ * @author WenTingTing by 2020/4/30
+ */
+public class ShellSort extends Sort {
+    public ShellSort(Integer[] array) {
+        super(array);
+    }
+
+    @Override
+    public void sort() {
+        // 步长序列
+        List<Integer> list = shellStepSequence();
+        // 分别对不同步长序列的子列进行排序
+        for (int step : list) {
+
+        }
+    }
+
+    /**
+     * 针对step步长进行排序
+     * 1.循环[0,step)位置元素index
+     * 2.对 index + step*n 位置的元素进行插入排序(操作同插入排序)，注意 不要数组越界
+     * 3.插入排序过程中，比较的最小元素位置不能小于index
+     *
+     * @param step
+     */
+    private void sort(int step) {
+        // 循环每个基本步长子序列
+        for (int i = 0; i < step; i++) {
+            int begin = i;
+            // 每一列元素的索引为：col、col+step、col+2*step、col+3*step
+            // 将每次增加固定步长的元素进行插入排序
+            while ((begin = begin + step) < array.length) {
+
+                // 元素的插入排序
+                // 备份待插入元素
+                int element = array[begin];
+                int cur = begin;
+                // 一直与前step步长的元素进行比较，如果小于当前元素则交换
+                while (cur > i && element < array[cur - step]) {// 后者比前者小，则交换位置，注意比较的最小元素不能小于i位置
+                    array[cur] = array[cur - step];
+                    cur -= step;
+                }
+                // 将元素插入合适位置
+                array[cur] = element;
+            }
+        }
+    }
+
+
+    /**
+     * 计算步长序列：
+     * 希尔规定的步长序列：n/(2^k)
+     * 步长序列元素从大到小(以1结尾)：[109,41,19,5,1]
+     *
+     * @return
+     */
+    private List<Integer> shellStepSequence() {
+        ArrayList<Integer> list = new ArrayList<>();
+        int step = array.length;// 兼顾数组长度为1的情况
+        while ((step >>= 1) > 0) {
+            list.add(step);
+        }
+        return list;
+    }
+
+
+}
